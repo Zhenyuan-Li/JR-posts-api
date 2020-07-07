332213fa96eb5bfc988a09961f478804adcd64b6
Vincent0Lee <vincent0lee95@gmail.com>
2020-07-07 23:01:07 +0800

    add helmet morgan cors pm2

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+
 import postRoutes from './routes/posts';
 
 dotenv.config();
+const port = process.env.PORT || 3000;
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-
+app.use(helmet());
+app.use(morgan('common'));
+app.use(cors());
 app.use(express.json());
+
 app.use('/posts', postRoutes);
 
 app.listen(port, () => {
